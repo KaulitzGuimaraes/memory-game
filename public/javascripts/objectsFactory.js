a21171d3753b43fb7b7beac11e598e5869b12af3
Kaulitz Guimaraes <v188530@g.unicamp.br>
2018-09-30 14:33:58 -0300

    adding pieces classes

--- public/javascripts/objectsFactory.js
+++ public/javascripts/objectsFactory.js
@@ -1,10 +1,57 @@
-function createApeace(image,playerOwn) {
+let gamePieces = ["apple.png","astah.jpg","bootstap.jpg","c.jpg",
+    "coursera.jpg","cpp.jpg","docker.png","github.jpg","ibm.jpg",
+    "java.png","javascript.png","jetbrains.png","php.png","pyhton.jpg",
+    "stackoverflow.png","swift.png","udemy.png","xcode.jpg"]
+let NUMBER_OF_PIECES = 36
+class Piece{
 
-    var peace = {
-        image : image,
-        playerOwn : playerOwn
+    constructor(coordinate){
+        this.coordinate = coordinate
+        this.isTurn = false
     }
-    return peace
+    turnPiece (){
+        this.isTurn = true
+    }
+    turnBackPiece (){
+        this.isTurn = false
+    }
+}
+class PairPieces {
+    constructor(imageName, coordinate1,coordinate2){
+        this.imageName = imageName
+        this.piece1 = new Piece(coordinate1)
+        this.piece2 = new Piece(coordinate2)
+    }
+
+    isPairMatch( coordinate1,coordinate2){
+        return (this.piece1.coordinate == coordinate1 & this.piece2 == coordinate2)
+
+    }
+
 
 }
 
+class Pieces{
+
+
+    constructor(tableSize){
+        this.tableSize = tableSize
+        this.pieces = new Set()
+
+    }
+    calculateRadomValue (){
+        return Math.floor((Math.random() * NUMBER_OF_PIECES) + 1)
+    }
+
+    random(){
+       var piecePos =  this.calculateRadomValue()
+        while(this.pieces.has(gamePieces[piecePos])){
+           piecePos = this.calculateRadomValue()
+        }
+
+    }
+    shufflePieces(){
+
+    }
+
+}
